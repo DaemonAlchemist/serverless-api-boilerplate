a9f83c30f03ce6c88f883bb542dd93670155bf0c
Andrew Wittrock <andy@wittrock.us>
2021-08-19 22:00:57 -0500

    Update CodedPromise function names

--- lib/coded-promise.ts
+++ lib/coded-promise.ts
@@ -4,12 +4,12 @@ import { withResponse } from "./withResponse";
 export const CodedPromise = <T = undefined, E = string>(f:(
     resolve:Func<T, void>,
     reject:Func<E, void>,
-    fail: Func<string, void>,
-    error: Func<string, void>,
+    invalidRequest: Func<string, void>,
+    internalError: Func<string, void>,
     notFound: Func<string, void>,
 ) => void):Promise<T> => new Promise((resolve, reject) => {
-    const fail = (msg:string) => reject(withResponse(400, msg));
-    const error = (msg:string) => reject(withResponse(500, msg));
+    const invalidRequest = (msg:string) => reject(withResponse(400, msg));
+    const internalError = (msg:string) => reject(withResponse(500, msg));
     const notFound = (msg:string) => reject(withResponse(404, msg));
-    f(resolve, reject, fail, error, notFound);
+    f(resolve, reject, invalidRequest, internalError, notFound);
 });
