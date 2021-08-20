367674e642d1d296adc4d08c371b60ed28aa7a65
Andrew Wittrock <andy@wittrock.us>
2021-08-20 12:51:55 -0500

    Export getHeader function

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ export { corsHeaders } from "./lib/config";
 export { dynamodb } from "./lib/dynamodb";
 export { catchErrors, error, getErrorMessage, isError } from "./lib/errors";
 export { createMappings, getMappings, ModuleMapDef, updateMappings } from "./lib/update-mappings";
-export { addNewId, getBody, getBodyParam, getEnv, getEvent, getPath, getPathParam, getQuery, makeHandler, pipeTo } from "./lib/util";
+export { addNewId, getBody, getBodyParam, getEnv, getEvent, getHeader, getPath, getPathParam, getQuery, makeHandler, pipeTo } from "./lib/util";
 export { withResponse } from "./lib/withResponse";
 export { del, get, patch, post, put } from "./lib/wrappers";
 
